e11340ead8255a8a2bfb7cb81b7c5c3cf5709990
sadra <sadrataghavi1383@gmail.com>
2021-05-05 15:42:18 +0430

    relocated js/css stuff

---
+++ base/static/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    brand_click()
+})
+
+function brand_click(){
+    $(".brand-img").click(function(){
+        window.open("https://github.com/SadraTghvi");
+    })
+}
