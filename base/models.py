3b1d72af671fbe6aac124d722b63a73ab77ce362
sadra <sadrataghavi1383@gmail.com>
2021-05-05 15:43:12 +0430

    updated models and linked to the admin page

--- base/models.py
+++ base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class lang(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
