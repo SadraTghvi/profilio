36b26a7f0e4c4627687414a2e1dc7522c0fda539
sadra <sadrataghavi1383@gmail.com>
2021-05-15 11:17:37 +0430

    debugging

--- base/views.py
+++ base/views.py
@@ -6,21 +6,9 @@ from django.http import JsonResponse
 # Create your views here.
 
 def main(request):
-    req = requests.get("https://api.github.com/users/SadraTghvi/repos")
-    if req.status_code == 200:
-        return JsonResponse({"x": req.json()})
     langs = Lang.objects.all()
     project = Project.objects.all()
     return render(request,"pages/index.html",{
         "items": langs,
         "projects": project,
     })
-
-'''
-await octokit.request('GET /repos/{owner}/{repo}', {
-  owner: 'octocat',
-  repo: 'hello-world'
-})
-'''
-
-
