07c0e5b1c7b968c75ca762e133018722af26990c
sadra <sadrataghavi1383@gmail.com>
2021-05-15 16:24:34 +0430

    added functionality for one of the js libraries

--- base/static/js/script.js
+++ base/static/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
     btn_click()
     tlit_card()
+    dark_mode()
+    $(".main").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
 })
 
 function btn_click(){
@@ -14,4 +22,21 @@ function btn_click(){
 
 function tlit_card(){
     $(".card").tilt({})
+}
+
+function dark_mode(){
+    $(".toggle__input").on("change",function(){
+        if ($(this).is(":checked")){
+            $("body").css("background-color","#0d1117")
+            $(".langs").css("color","#f0f6fc")
+            $(".card").css("background-color","#808080")
+            $(".card_title").css("color","#000000")
+            
+        } else {
+            $("body").css("background-color","")
+            $(".card").css("background-color","")
+            $(".card_title").css("color","")
+        }
+
+    })
 }
